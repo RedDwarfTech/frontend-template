9d85a1074760bbfa8935e7218b922e5b7070112e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-10 00:09:40 +0800

    feat: initial

---
+++ src/page/home/App.tsx
@@ -0,0 +1,13 @@
+import withConnect from '@/component/withConnect';
+import './App.css'
+
+const App: React.FC = () => {
+
+  return (
+    <div>
+     sdfwegwegew
+    </div>
+  )
+}
+
+export default withConnect(App);
